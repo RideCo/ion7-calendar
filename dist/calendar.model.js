7ec26d20de7b2d956a7888b1c03cfd4b8031e0cc
khalid.walkermohamed <khalid.walkermohamed@rideco.com>
2026-06-17 16:39:39 -0400

    feat: Add today in aria-label for current date button

--- dist/calendar.model.js
+++
@@ -1,7 +0,0 @@
-export class CalendarMonth {
-}
-export class CalendarResult {
-}
-export class CalendarComponentMonthChange {
-}
-//# sourceMappingURL=calendar.model.js.map
--- dist/services/calendar-options.provider.js
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from '@angular/core';
-export const DEFAULT_CALENDAR_OPTIONS = new InjectionToken('DEFAULT_CALENDAR_MODAL_OPTIONS');
-//# sourceMappingURL=calendar-options.provider.js.map
